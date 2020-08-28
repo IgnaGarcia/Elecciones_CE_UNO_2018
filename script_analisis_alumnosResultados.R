44547bd3eca6293b81a70a58e3ed9ff4a0ceb199
Igna <gnachoxp@gmail.com>
2020-08-28 19:27:00 -0300

    vistas

--- script_analisis_alumnosResultados.R
+++ script_analisis_alumnosResultados.R
@@ -1,5 +1,8 @@
 #Analisis de resultados en relacion al total de alumnos
-
+library(readr)
+library(ggplot2)
+library(plotly)
+library(tidyr)
 ##### Cargar datos
 padron_2019 <- read_delim("data_padron_2019.csv", ";", escape_double = FALSE, trim_ws = TRUE)
 resultados_2018 <- read_delim("data_resultados_2018.csv", ";", escape_double = FALSE, locale = locale(decimal_mark = ","), trim_ws = TRUE)
@@ -50,11 +53,98 @@ porcentNU <- c(jujuy_nuVotantes, cente_nuVotantes, cba_nuVotantes, nuVotantes)
 
 ### Variables traidas
 ID <- resultados_2018$ID 
-Escuela <- resultados_2018$Escuela
+Escuela <- as.factor(resultados_2018$Escuela)
 Votantes <- resultados_2018$totalVotos
+
 ##### Cargar datos en un Data Frame
 data <- data.frame(ID, Escuela, Alumnos, Votantes, porcentVotantes, porcentGEA, porcentNU)
+
+##Columna nueva para no votantes
+data$NoVotantes <- 100 - data$porcentVotantes
 
 
 ##### Guardar en archivo .csv
 write.csv2(data, "data_alumnosResultados.csv",  row.names = FALSE, fileEncoding = "UTF-8")
+
+
+fig <- plot_ly(data, x = ~Escuela) %>%
+  add_trace(y = ~porcentGEA, name = 'GEA', width= .3, 
+            text = porcentGEA, textposition = 'auto',
+            marker = list(color = 'rgb(44, 160, 44)')) %>% 
+  add_trace(y = ~porcentNU, name = 'NU', width= .3, 
+            text = porcentNU, textposition = 'auto',
+            marker = list(color = 'rgb(31, 119, 180)')) %>%
+  layout(title = "Resultados Elecciones CE UNO",
+         xaxis = list(title = ""),
+         yaxis = list(title = "", range= c(0,100)))
+fig
+
+fig2 <- plot_ly(data, x = ~Escuela) %>%
+  add_trace(y = ~porcentGEA, name = 'GEA', width= .3,
+            text = porcentGEA, textposition = 'auto',
+            marker = list(color = 'rgb(44, 160, 44)')) %>% 
+  add_trace(y = ~porcentNU, name = 'NU', width= .3, 
+            text = porcentNU, textposition = 'auto',
+            marker = list(color = 'rgb(31, 119, 180)')) %>%
+  layout(title = "Resultados Elecciones CE UNO",
+         xaxis = list(title = ""),
+         yaxis = list(title = "", range= c(0,100)),
+         barmode = 'stack')
+fig2
+
+fig1 <- plot_ly(data, x = ~Escuela) %>%
+  add_trace(y = ~porcentNU, name = 'NU', width= .2, 
+            text = porcentNU, textposition = 'auto',
+            marker = list(color = 'rgb(31, 119, 180)')) %>%
+  add_trace(y = ~NoVotantes, name = 'Ausentes', width= .2,
+            text = NoVotantes, textposition = 'auto',
+            marker = list(color = 'rgb(204,204,204)')) %>%
+  add_trace(y = ~porcentGEA, name = 'GEA', width= .2, 
+            text = porcentGEA, textposition = 'auto',
+            marker = list(color = 'rgb(44, 160, 44)')) %>% 
+  layout(title = "Resultados Elecciones CE UNO",
+         xaxis = list(title = ""),
+         yaxis = list(title = ""))
+fig1
+
+fig3 <- plot_ly(data, x = ~Escuela) %>%
+  add_trace(y = ~porcentGEA, name = 'GEA', width= .3, 
+            text = porcentGEA, textposition = 'auto',
+            marker = list(color = 'rgb(44, 160, 44)')) %>% 
+  add_trace(y = ~porcentNU, name = 'NU', width= .3, 
+            text = porcentNU, textposition = 'auto',
+            marker = list(color = 'rgb(31, 119, 180)')) %>%
+  add_trace(y = ~NoVotantes, name = 'Ausentes', width= .3,
+            text = NoVotantes, textposition = 'auto',
+            marker = list(color = 'rgb(204,204,204)')) %>%
+  layout(title = "Resultados Elecciones CE UNO",
+         xaxis = list(title = ""),
+         yaxis = list(title = ""),
+         barmode = 'stack')
+fig3
+
+data1 <- subset(data, Escuela != "Total")
+
+fig4 <- plot_ly(data1, labels = ~Escuela, values = ~porcentGEA, type = 'pie') %>% 
+  layout(title = 'Votantes de GEA',
+         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+fig4
+
+fig5 <- plot_ly(data1, labels = ~Escuela, values = ~porcentNU, type = 'pie') %>% 
+  layout(title = 'Votantes de NU',
+         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+fig5
+
+data2 <- subset(data, Escuela == "Total")
+data2 <- data2 %>% gather(seccion, porcent, 6:ncol(data2))
+
+fig4 <- plot_ly(data2, labels = ~seccion, values = ~porcent, type = 'pie') %>% 
+  layout(title = 'Votantes Totales',
+         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+fig4
